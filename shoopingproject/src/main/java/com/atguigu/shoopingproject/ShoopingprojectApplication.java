76ffcd246913ce36c401b02be7b8de59779dfc4c
Administrator <1817512531@qq.com>
2019-10-22 16:02:05 +0800

    shoopingproject

---
+++ shoopingproject/src/main/java/com/atguigu/shoopingproject/ShoopingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.shoopingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoopingprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoopingprojectApplication.class, args);
+    }
+
+}
